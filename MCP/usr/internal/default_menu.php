0aebccf8d1d3da969d3c8ea5b6e9c582f8f17a31
Forecaster <forecaster@towerofawesome.org>
2021-04-27 20:05:56 +0200

    - Add new issue link - Add upload limitation to file selector description - Add hover tooltips to menu items

--- MCP/usr/internal/default_menu.php
+++ MCP/usr/internal/default_menu.php
@@ -5,9 +5,9 @@ $menu_root = (defined('SITE_ROOT_URL') ? "/" . SITE_ROOT_URL : "");
 
 $menu = new MenuStructure();
 
-$menu->AddItem(new MenuLink("Home", $menu_root));
-$menu->AddItem(new MenuLink("GitHub", "https://github.com/Forecaster/UniversalVTTExport_to_FGModule"));
-$menu->AddItem(new MenuLink("DungeonFog", "https://dungeonfog.com"));
-$menu->AddItem(new MenuLink("DF Discord", "https://dungeonfog.com/discord"));
+$menu->AddItem(new MenuLink("Home", $menu_root, "Reload this page"));
+$menu->AddItem(new MenuLink("GitHub", "https://github.com/Forecaster/UniversalVTTExport_to_FGModule", "The GitHub repository for this project"));
+$menu->AddItem(new MenuLink("DungeonFog", "https://dungeonfog.com", "DungeonFog Website"));
+$menu->AddItem(new MenuLink("DF Discord", "https://dungeonfog.com/discord", "DungeonFog Discord Server"));
 
 return $menu;
